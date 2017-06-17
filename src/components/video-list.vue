f13ece9e1cde51166acecdc4b9a1276ccfe11070
Andiedie <zchangan@163.com>
2017-06-17 13:09:23 +0800

    避免了并发请求channel的问题

--- src/components/video-list.vue
+++ src/components/video-list.vue
@@ -1,44 +1,43 @@
 <template lang="html">
   <div class="video-list">
-    <swiper :options="swiperOption" ref="swiper">
-      <swiper-slide class="menu">Menu slide</swiper-slide>
+    <swiper :options="menuSwiperOption" ref="swiper">
+      <swiper-slide class="channel-list">
+        <channel-list>
+        </channel-list>
+      </swiper-slide>
       <swiper-slide>
-        <div class="menu-button" :class="{ 'cross': menuCross }" @click="toggleMenu">
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-        </div>
-        <swiper :options="swiperOption1">
-          <swiper-slide v-for="(n,index) in 10">
-            {{index}}
+        <swiper :options="videoListSwiperOption">
+          <swiper-slide v-for="video in videoList" :key="video.id">
+            <img class="video-img" v-lazy="video.img">
           </swiper-slide>
         </swiper>
-        <!-- <swiper :options="swiperOption1">
-          <swiper-slide v-for="video in videoList" :key="video.id">
-            {{video.id}}
-          </swiper-slide>
-        </swiper> -->
       </swiper-slide>
     </swiper>
   </div>
 </template>
 
 <script>
+import channelList from './channel-list.vue';
 export default {
   data () {
     return {
-      menuCross: false,
       videoList: [],
-      swiperOption: {
+      menuSwiperOption: {
         slidesPerView: 'auto',
         initialSlide: 1,
-        resistanceRatio: 0.00000000000001,
+        resistanceRatio: 0.1,
         slideToClickedSlide: true
       },
-      swiperOption1: {
-        direction: 'vertical'
+      videoListSwiperOption: {
+        direction: 'vertical',
+        height: 300,
+        centeredSlides: true,
+        spaceBetween: 20
       }
     };
+  },
+  components: {
+    'channel-list': channelList
   },
   computed: {
     swiper () {
@@ -59,81 +58,11 @@ export default {
 </script>
 
 <style lang="scss">
-.menu {
+.channel-list {
   min-width: 100px;
-  width: 70%;
-  max-width: 320px;
-  background-color: #2C8DFB!important;
-  color: #fff;
+  width: 20%;
 }
-.content {
-  width: 100%;
-}
-.menu-button {
-  position: absolute;
-  top: 0px; left: 0px;
-  padding: 15px;
-  cursor: pointer;
-  -webkit-transition: .3s;
-  transition: .3s;
-  background-color: #2C8DFB;
-  /*margin: 14px;
-  border-radius: 5px;*/
-}
-.menu-button .bar:nth-of-type(1) {
-  margin-top: 0px;
-}
-.menu-button .bar:nth-of-type(3) {
-  margin-bottom: 0px;
-}
-.bar {
-  position: relative;
-  display: block;
-  width: 50px;
-  height: 5px;
-  margin: 10px auto;
-  background-color: #fff;
-  border-radius: 10px;
-  -webkit-transition: .3s;
-  transition: .3s;
-}
-.menu-button:hover .bar:nth-of-type(1) {
-  -webkit-transform: translateY(1.5px) rotate(-4.5deg);
-  -ms-transform: translateY(1.5px) rotate(-4.5deg);
-  transform: translateY(1.5px) rotate(-4.5deg);
-}
-.menu-button:hover .bar:nth-of-type(2) {
-  opacity: .9;
-}
-.menu-button:hover .bar:nth-of-type(3) {
-  -webkit-transform: translateY(-1.5px) rotate(4.5deg);
-  -ms-transform: translateY(-1.5px) rotate(4.5deg);
-  transform: translateY(-1.5px) rotate(4.5deg);
-}
-.cross .bar:nth-of-type(1) {
-  -webkit-transform: translateY(15px) rotate(-45deg);
-  -ms-transform: translateY(15px) rotate(-45deg);
-  transform: translateY(15px) rotate(-45deg);
-}
-.cross .bar:nth-of-type(2) {
-  opacity: 0;
-}
-.cross .bar:nth-of-type(3) {
-  -webkit-transform: translateY(-15px) rotate(45deg);
-  -ms-transform: translateY(-15px) rotate(45deg);
-  transform: translateY(-15px) rotate(45deg);
-}
-.cross:hover .bar:nth-of-type(1) {
-  -webkit-transform: translateY(13.5px) rotate(-40.5deg);
-  -ms-transform: translateY(13.5px) rotate(-40.5deg);
-  transform: translateY(13.5px) rotate(-40.5deg);
-}
-.cross:hover .bar:nth-of-type(2) {
-  opacity: .1;
-}
-.cross:hover .bar:nth-of-type(3) {
-  -webkit-transform: translateY(-13.5px) rotate(40.5deg);
-  -ms-transform: translateY(-13.5px) rotate(40.5deg);
-  transform: translateY(-13.5px) rotate(40.5deg);
+.video-img {
+  height: 100%;
 }
 </style>
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,17 +1,24 @@
 import iqiyi from '@/utils/iqiyi';
+let updateChannels;
 
 export default {
   async updateChannels ({commit}) {
-    let channels = await iqiyi.getChannels();
+    if (updateChannels) return updateChannels;
+    console.log('updateChannels');
+    updateChannels = iqiyi.getChannels();
+    let channels = await updateChannels;
+    updateChannels = null;
     commit('updateChannels', channels);
   },
   async updateRecommend ({commit}) {
+    console.log('updateRecommend');
     let recommends = await iqiyi.getRecommends();
     commit('updateCarousel', recommends[0].video_list);
     recommends.splice(0, 1);
     commit('updateRecommend', recommends);
   },
   async updateVideoList ({state, commit, dispatch}) {
+    console.log('updateVideoList');
     if (!state.channels.length) {
       await dispatch('updateChannels');
     }
